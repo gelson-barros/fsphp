5e6a79a021a516679a20cd065e314588a8c27853
geferreira <61064472+nicosbrilho@users.noreply.github.com>
2020-05-24 15:45:05 +0100

    termino da aula 03-03

--- 03-manipulacao-e-tratamento/03-04-manipulacao-de-objetos/index.php
+++ 03-manipulacao-e-tratamento/03-04-manipulacao-de-objetos/index.php
@@ -40,3 +40,23 @@ var_dump($company);
  * [ análise ] class | objetcs | instances
  */
 fullStackPHPClassSession("análise", __LINE__);
+
+$date = new DateTime();
+
+var_dump([
+    "class " => get_class($date),
+    "methods" => get_class_methods($date),
+    "vars" => get_object_vars($date),
+    "parent" => get_parent_class($date),
+    "subclass" => is_subclass_of($date, "DAteTime"),
+]);
+
+$expetion = new PDOException();
+
+var_dump([
+    "class " => get_class($expetion),
+    "methods" => get_class_methods($expetion),
+    "vars" => get_object_vars($expetion),
+    "parent" => get_parent_class($expetion),
+    "subclass" => is_subclass_of($expetion, "Exception"),
+]);
